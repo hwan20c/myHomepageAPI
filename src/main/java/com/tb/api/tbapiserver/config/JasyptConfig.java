ccaa13777ab95dc8767692500e70e50d4983d988
hwan20c <hwan20c@gmail.com>
2022-10-21 17:31:14 +0900

    jasypt change + gitignore update

--- src/main/java/com/tb/api/tbapiserver/config/JasyptConfig.java
+++ src/main/java/com/tb/api/tbapiserver/config/JasyptConfig.java
@@ -1,21 +1,25 @@
 package com.tb.api.tbapiserver.config;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Collectors;
+
 import org.jasypt.encryption.StringEncryptor;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 public class JasyptConfig {
-
-  private static final String ENCRYPT_KEY = "HWAN@1218";
 
   @Bean("jasyptStringEncryptor")
   public StringEncryptor stringEncryptor() {
     PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
     SimpleStringPBEConfig config = new SimpleStringPBEConfig();
-    config.setPassword(ENCRYPT_KEY);
+    config.setPassword(getJasyptPassword());
     config.setAlgorithm("PBEWithMD5AndDES");
     config.setKeyObtentionIterations("1000");
     config.setPoolSize("1");
@@ -24,6 +28,16 @@ public class JasyptConfig {
     config.setStringOutputType("base64");
     encryptor.setConfig(config);
     return encryptor;
+  }
+
+  private String getJasyptPassword() {
+    try {
+      ClassPathResource resource = new ClassPathResource("jasypt_password.txt");
+      return Files.readAllLines(Paths.get(resource.getURI())).stream()
+              .collect(Collectors.joining(" "));
+    } catch (IOException e) {
+      throw new RuntimeException("Not found Jasypt password");
+    }
   }
   
 }
